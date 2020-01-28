c45b54f227973d768bd346cccd4156149125a6f7
Johan <johanvilela@hotmail.com>
2020-01-28 14:41:35 -0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+// MogoDB Local
+// const mongoDB = 'mongodb://127.0.0.1/my_database';
+
+// MogoDB Atlas
+// const mongoDB = 'mongodb+srv://omnistack:omnistack@cluster0-wmkd0.mongodb.net/week10?retryWrites=true&w=majority';
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
