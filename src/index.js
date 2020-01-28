a9fa84e50934306c97a105387d1481382f7b4ca8
Johan <johanvilela@hotmail.com>
2020-01-28 14:56:29 -0300

    Clean index.js

--- src/index.js
+++ src/index.js
@@ -13,12 +13,6 @@ const server = http.Server(app);
 
 setupWebsocket(server);
 
-// MogoDB Local
-// const mongoDB = 'mongodb://127.0.0.1/my_database';
-
-// MogoDB Atlas
-// const mongoDB = 'mongodb+srv://omnistack:omnistack@cluster0-wmkd0.mongodb.net/week10?retryWrites=true&w=majority';
-
 mongoose.connect(
   process.env.MONGO_URL,
   {
